ed7d03c7c398d0ed7aba1418abfedfca7c677898
Triborda <em17163@outlook.com>
2025-03-11 08:10:57 +1300

    added custom recipe (to figure out how it works) 4 dirt clumps in a square make a dirt block (mirroring clay)

--- src/main/java/net/triborda/dirtplanemod/datagen/ModRecipeProvider.java
+++ src/main/java/net/triborda/dirtplanemod/datagen/ModRecipeProvider.java
@@ -2,8 +2,12 @@ package net.triborda.dirtplanemod.datagen;
 
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.triborda.dirtplanemod.item.ModItems;
 
 import java.util.function.Consumer;
 
@@ -13,7 +17,12 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     }
 
     @Override
-    protected void buildRecipes(Consumer<FinishedRecipe> consumer) {
-
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, Items.DIRT)
+                .pattern("DD")
+                .pattern("DD")
+                .define('D', ModItems.DIRT_CLUMP.get())
+                .unlockedBy(getHasName(ModItems.DIRT_CLUMP.get()), has(ModItems.DIRT_CLUMP.get()))
+                .save(pWriter);
     }
 }
